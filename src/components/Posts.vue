db556155b12a0a17c859be1134001eebf23ba9bb
Luan Cardoso <luanleitecardoso@gmaili.com>
2020-03-31 17:38:41 -0300

    Registering a component

---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<p>HELLO</p>
+	</div>
+</template>
+
+<script>
+    export default {
+      name: 'PostsExemple',
+      data() {
+        return {
+
+        }
+      }
+    };
+</script>
+
+<style>
+</style>
